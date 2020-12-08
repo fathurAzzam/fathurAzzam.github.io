67740d0c989fc4972141c3159d1a3bb4298b99c1
fathurAzzam <72847383+fathurAzzam@users.noreply.github.com>
2020-12-08 11:31:45 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function salam ()
+{
+    alert("assalamulaikum Saudaraku !");
+}
+function salamKenal (teman){
+    alert("apa kabar" + teman);
+}
+function jumlah(a,b){
+    let c = a + b ;
+    return c;
+}
